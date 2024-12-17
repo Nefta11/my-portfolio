16334bc6c5535e51cd8759827f460dfe0231b555
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-17 15:05:57 -0600

    Agregar estilos de máscara y mejorar la estructura del portafolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode:'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            maskImage: {
+                'gradient-black-transparent': 'linear-gradient(black 80%, transparent)',
+            },
+        },
+    },
+    plugins: [
+        function ({ addUtilities }) {
+            addUtilities({
+                '.mask-gradient-black-transparent': {
+                    maskImage: 'linear-gradient(black 50%, transparent)',
+                },
+            });
+        },
+    ],
 }
